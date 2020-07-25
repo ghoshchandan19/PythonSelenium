5db783f868d7e0c581698b5e9365479a0f1259ac
chandan <chandan.ghosh@tothenew.com>
2020-07-25 11:15:43 +0530

    case conversion of strings

---
+++ BasicPython/Strings/StringConversion.py
@@ -0,0 +1,10 @@
+s="chandan Ghosh"
+print(s.lower())# converts all the characters into lowercased
+print(s.upper())# converts all the characters into uppercase
+print(s.title()) # converts the first character of each word to upper case
+print(s.capitalize())#converts the first character of the string to upper case
+print(s.swapcase())# converts the lower case to uppercase and vice versa
+
+#check starting and ending part of the stringsplit
+print(s.startswith("chan"))
+print(s.endswith("chan"))
