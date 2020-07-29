58c0970d9eadcae40072e5a0c80b038fc7f39785
chandan <ghoshchandan19@gmail.com>
2020-07-29 18:01:39 +0530

    Added tuple methods

--- BasicPython/Tuples/tuplearihematicoperations.py
+++ BasicPython/Tuples/tuplearihematicoperations.py
@@ -11,4 +11,32 @@ print(t.index(10))
 
 print(sorted(t))
 
+#Min and max of tuple
+t1=(30,10,40,50,70)
+print(min(t1))
+print(max(t1))
+
+t2='durga'
+print(min(t2))
+print(max(t2))
+
+#Comparing two from_tuples
+t1=(10,20,30)
+t2=(40,50,60)
+print(t1<t2)
+
+#Tuple packing and Unpacking
+a=10
+b=20
+c=30
+d=40
+t=a,b,c,d
+print(t)
+
+t=(60,90,70,80)
+a,b,c,d=t
+print(a)
+print(b)
+print(c)
+print(d)
 
---
+++ BasicPython/Tuples/tuplemethods.py
@@ -0,0 +1,15 @@
+#deleting a tuple
+tuple=('p','q',)
+print(tuple)
+del(tuple)
+print(tuple)
+
+#Tuple membership
+tuple=('p','q','r','s','t')
+print('p' in tuple)
+print('u' in tuple)
+
+#iterating through tuples
+for name in ('John','Karate'):
+    print('Hello',name)
+
